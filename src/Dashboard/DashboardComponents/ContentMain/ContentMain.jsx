9b44b97e51ee11c1d9097a51536e55a07765bd0d
adeyemijadesolajohn-dev <jcrown47500@gmail.com>
2025-04-21 13:17:03 +0100

    correction

--- src/Dashboard/DashboardComponents/ContentMain/ContentMain.jsx
+++ src/Dashboard/DashboardComponents/ContentMain/ContentMain.jsx
@@ -1,44 +1,11 @@
-import { useState, useRef, useEffect } from "react";
 import "./ContentMain.scss";
 import Cards from "../Cards/Cards";
 import Transactions from "../Transactions/Transactions";
 import Report from "../Report/Report";
 import Usage from "../Usage/Usage";
-import "react-date-range/dist/styles.css";
-import "react-date-range/dist/theme/default.css";
-import { addDays } from "date-fns";
-import { DateRange } from "react-date-range";
+import DateRangeHolder from "./DateRangeHolder";
 
 const ContentMain = () => {
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
-      key: "selection",
-    },
-  ]);
-
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const dateRangeRef = useRef(null);
-
-  const handleInputClick = () => {
-    setShowDatePicker(true);
-  };
-
-  const handleOutsideClick = (event) => {
-    if (dateRangeRef.current && !dateRangeRef.current.contains(event.target)) {
-      setShowDatePicker(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick);
-
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, []);
-
   return (
     <div className="mainContentHolder">
       <div className="contentGridOne">
@@ -54,23 +21,7 @@ const ContentMain = () => {
           </div>
 
           <div className="midContentHeader gridCommon">
-            <div className="dateRangeHolder">
-              <div
-                ref={dateRangeRef}
-                className={`dateRangeWrapper ${
-                  showDatePicker ? "hide-date-range" : ""
-                }`}
-                onClick={handleInputClick}
-              >
-                <DateRange
-                  editableDateInputs={true}
-                  onChange={(item) => setState([item.selection])}
-                  moveRangeOnFirstSelection={false}
-                  ranges={state}
-                  showMonthAndYearPickers={false}
-                />
-              </div>
-            </div>
+            <DateRangeHolder />
           </div>
         </div>
       </div>
